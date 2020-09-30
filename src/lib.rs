065dada071d1138f350a8312faa8b4291ab2f41c
SLourenco <sergio.santos.lourenco@protonmail.com>
2020-09-30 13:44:23 +0100

    Expose extract feature in library

--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,11 @@ pub mod country;
 mod validator;
 
 pub struct Citizen {
-    gender: char,
-    year_of_birth: i32,
-    month_of_birth: Option<u8>,
-    day_of_birth: Option<u8>,
-    place_of_birth: Option<String>,
+    pub gender: char,
+    pub year_of_birth: i32,
+    pub month_of_birth: Option<u8>,
+    pub day_of_birth: Option<u8>,
+    pub place_of_birth: Option<String>,
 }
 
 pub fn validate_id(id: &str, country: country::Code) -> bool {
@@ -16,11 +16,7 @@ pub fn validate_id(id: &str, country: country::Code) -> bool {
     return country_validator.validate_id(id);
 }
 
-
-// #[cfg(test)]
-// mod tests {
-//     #[test]
-//     fn it_works() {
-//         assert_eq!(2 + 2, 4);
-//     }
-// }
+pub fn extract_information(id: &str, country: country::Code) -> Option<Citizen> {
+    let country_validator = validator::get_validator(country);
+    return country_validator.extract_citizen(id);
+}
--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -20,3 +20,11 @@ fn validate_id_france_invalid_test() {
     assert!(!socrates::validate_id("123X123X123dDAS", socrates::country::Code::FR));
 }
 
+#[test]
+fn extract_france_test() {
+    let citizen = socrates::extract_information("2820819398814 09", socrates::country::Code::FR).unwrap();
+    assert_eq!(citizen.gender, 'F');
+    assert_eq!(citizen.year_of_birth, 1982);
+    assert_eq!(citizen.month_of_birth.unwrap(), 8);
+    assert_eq!(citizen.place_of_birth.unwrap(), "Corrèze");
+}
